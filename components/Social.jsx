daba5223ce9903a58d4ffde6a17e1012700301ef
Ange Gardien <angegirishyaka@gmail.com>
2024-10-27 23:24:40 +0200

    working on my portfolio1

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,36 +1,34 @@
 import Link from "next/link";
-
 import { FaGithub, FaLinkedin, FaYoutube, FaTwitter } from "react-icons/fa";
 
 const socials = [
-  {
-    icon: <FaGithub />,
-    path: "https://github.com/ange-gardien1"
-  },
+  { icon: <FaGithub />, path: "https://github.com/ange-gardien1" },
   {
     icon: <FaLinkedin />,
-    path: "https://www.linkedin.com/in/ange-gardien-144a0018b/"
+    path: "https://www.linkedin.com/in/ange-gardien-144a0018b/",
   },
-  {
-    icon: <FaYoutube />,
-    path: "",
-  },
-  {
-    icon: <FaTwitter />,
-    path: ""
-  },
+  { icon: <FaYoutube />, path: "" }, 
+  { icon: <FaTwitter />, path: "" },
 ];
 
 const Social = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
-      {socials.map((item, index) => {
-        return (
-          <Link key={index} href={item.path} className={iconStyles}>
-            {item.icon}
-          </Link>
-        );
-      })}
+      {socials.map(
+        (item, index) =>
+          item.path && (
+            <Link
+              key={index}
+              href={item.path}
+              className={iconStyles}
+              aria-label={`Link to ${item.path}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </Link>
+          )
+      )}
     </div>
   );
 };
