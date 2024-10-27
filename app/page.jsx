a597de1df7c90040bb65e361a3f37ff283f6253b
Ange Gardien <angegirishyaka@gmail.com>
2024-10-28 00:06:47 +0200

    social medi fixing

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
-// import Photo from "@/components/photo";
-// import Social from "@/components/social";
+
+
+import Social from "@/components/Social";
 import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
@@ -32,12 +33,10 @@ export const Home = () => {
                 <FiDownload className="text-xl" />
               </Button>
               <div className="mb-8 xl:mb-0">
-                {/* <Social
-                  containerStyles="flex gap-6"
+                <Social containerStyles="flex gap-6"
                   iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center
                    items-center text-accent text-base 
-                  hover:bg-accent hover:text-primary hover:transition-all duration-500"
-                /> */}
+                  hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
               </div>
             </div>
           </div>
